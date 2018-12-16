1f9d8c1e8faaf22180f12b173263632cf2235138
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-15 19:47:06 -0700

    navbar routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
+import Navbar from './Navbar'
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={Home}/>
-        <Route path="/players" component={Players}/>
-        <Route path="/teams" component={Teams}/>
+        <div>
+          <Navbar />
+          
+          <Route exact path="/" component={Home}/>
+          <Route path="/players" component={Players}/>
+          <Route path="/teams" component={Teams}/>
+        </div>
       </Router>
     )
   }
