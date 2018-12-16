d3097d8c8e2fed86065f32f6765c0fceb3291cc0
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-15 19:51:48 -0700

    404

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
@@ -11,10 +11,13 @@ class App extends React.Component {
       <Router>
         <div>
           <Navbar />
-          
-          <Route exact path="/" component={Home}/>
-          <Route path="/players" component={Players}/>
-          <Route path="/teams" component={Teams}/>
+
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/players" component={Players}/>
+            <Route path="/teams" component={Teams}/>
+            <Route render={() => <h1 className='text-center'>Four oh Four</h1>} />
+          </Switch>
         </div>
       </Router>
     )
